d968fac8cab60b558e6d9b4984c72e13eb8f320e
AutoAkshay <akshaykalam93@gmail.com>
2024-09-08 10:13:14 +0530

    conflict

--- codeStudio/src/test/java/parsing/NewClassforGit.java
+++ codeStudio/src/test/java/parsing/NewClassforGit.java
@@ -116,8 +116,7 @@ public class NewClassforGit {
 
 	@Test
 	public void test2() throws JsonProcessingException {
-		System.out.println("Adding new file");
-		System.out.println("Adding new file");
+		System.out.println("Adding duplicate new file");
 	}
 
 }
